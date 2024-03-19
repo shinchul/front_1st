04b692b483dd52f18306fa2b2cdd4c56badb5600
brian90shin <brain90shin@gmail.com>
2024-03-19 20:11:27 +0900

    2주차 과제 A 제출

--- README.md
+++ README.md
@@ -5,3 +5,63 @@
 1. 자바스크립트 기본기 다지기
    1. [프로토타입과 V8엔진 히든클래스 이해하기](./packages/chapter1/src/a.js)
    2. [MicroTask 큐, MacroTask 큐, 애니메이션 등 비동기 메커니즘 이해하기](./packages/chapter1/src/b.js)
+
+## chapt1 
+# 과제 A 해결
+<!-- Worker.prototype.getHealth = function () {
+  return this._health;
+}
+
+Worker.prototype.work = function () {
+  this._health--;
+}
+
+// Object.create을 사용했는데, setPrototypeOf를 사용해도 되고 new 키워드를 사용해도 무방합니다. (new 키워드는 불필요한 처리가 추가로 들어가게 되겠지만요)
+// 프로토타입 슬롯을 갖는 객체를 prototype에 할당함으로써 체이닝을 구성할 수 있다는 점만 이해하면 됩니다.
+JuniorEngineer.prototype = Object.create(Worker.prototype, {});
+
+JuniorEngineer.prototype._super = function (health) {
+  // 부모 함수를 반영하기 위해 call 함수를 이용하고 있습니다.
+  // 이 로직을 이해하기 위해서는 자바스크립트 함수의 동작과 this 바인딩 개념을 알아야 합니다.
+  Worker.call(this, health);
+}
+
+JuniorEngineer.prototype.getIntelligence = function () {
+  return this._intelligence;
+}
+
+JuniorEngineer.prototype.work = function () {
+  // 만약 메서드 명이 달랐다면 this 키워드로 체이닝을 통해 바로 접근이 가능 했겠지만
+  // 메서드 명이 같아서 아래와 같이 실행해줄 수 있습니다.
+  Worker.prototype.work.call(this);
+  this._intelligence++;
+}
+
+JuniorEngineer.prototype.isBornGenius = function () {
+  return this._isBornGenius ?? false;
+}  -->
+
+# 과제 B 해결
+<!-- do() {
+    const taskBundleSize = 100;
+    const taskBundleCount = Math.ceil(this._tasks.length / taskBundleSize);
+
+    // 비동기로 여러 작업을 분할한다음 순서를 보장하기 위해서
+    // Promise chain을 사용합니다.
+    let taskChain = Promise.resolve(0);
+    for (let i = 0; i < taskBundleCount; i++) {
+      taskChain = taskChain.then(next => this._do(next, next + taskBundleSize));
+    }
+
+    return taskChain;
+  } -->
+
+  # 과제 C 해결
+# 히든클래스 활용
+# 히든 클래스 구조가 변화하지 않도록 만들어서 내부적으로 인스턴스가 재생성되는 부하를 줄입니다.
+<!-- function JuniorEngineer(health, intelligence) {
+  this._super(health);
+  this._intelligence = intelligence ?? 1;
+  // 문제 C
+  this._isBornGenius = this._intelligence > 10;
+} -->
--- packages/chapter2/src/render.js
+++ packages/chapter2/src/render.js
@@ -1,44 +1,167 @@
 export function jsx(type, props, ...children) {
-  return {}
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) => {
+        if (typeof child === 'function') return child()
+        return child
+      }),
+    },
+  }
 }
 
 export function createElement(node) {
-  // jsx를 dom으로 변환
+  /*
+  // 만약 node가 문자열이라면 텍스트 노드로 생성
+  if (typeof node === 'string') {
+    return document.createTextNode(node);
+  }
+  
+  // 그 외에는 element 노드 생성
+  const element = document.createElement(node.type);
+  
+  // 속성 설정
+  updateAttributes(element, node.props);
+  
+  // 자식 노드 추가
+  node.children.forEach(child => {
+    const childElement = createElement(child);
+    element.appendChild(childElement);
+  });
+  
+  return element;
+  */
+  const el = document.createElement(node.type)
+
+  const { children, ...otherProps } = node.props
+  if (otherProps !== null) {
+    updateAttributes(el, {}, otherProps)
+  }
+
+  if (Array.isArray(children)) {
+    children.forEach((child) => {
+      typeof child === 'string' ? (el.innerHTML = child) : el.appendChild(createElement(child))
+    })
+  }
+  return el
 }
 
 function updateAttributes(target, newProps, oldProps) {
+  /*
   // newProps들을 반복하여 각 속성과 값을 확인
   //   만약 oldProps에 같은 속성이 있고 값이 동일하다면
   //     다음 속성으로 넘어감 (변경 불필요)
   //   만약 위 조건에 해당하지 않는다면 (속성값이 다르거나 구속성에 없음)
   //     target에 해당 속성을 새 값으로 설정
+  if (newProps) {
+    for (const [key, value] of Object.entries(newProps)) {
+      if (oldProps && oldProps[key] === value) {
+        // 새로운 속성이 이전과 동일하다면 변경 불필요
+        continue;
+      }
+      target.setAttribute(key, value); // 새로운 속성으로 설정
+    }
+  }
 
   // oldProps을 반복하여 각 속성 확인
   //   만약 newProps들에 해당 속성이 존재한다면
   //     다음 속성으로 넘어감 (속성 유지 필요)
   //   만약 newProps들에 해당 속성이 존재하지 않는다면
   //     target에서 해당 속성을 제거
+  if (oldProps) {
+    for (const key in oldProps) {
+      if (!(key in newProps)) {
+        // 이전 속성이 새로운 속성에 없다면 제거합니다.
+        target.removeAttribute(key);
+      }
+    }
+  }
+  */
+  Object.keys(newProps).forEach((prop) => {
+    if (prop !== 'children' && newProps[prop] !== oldProps[prop]) {
+      target.setAttribute(prop, newProps[prop])
+    }
+  })
+  Object.keys(oldProps).forEach((prop) => {
+    if (!(prop in newProps) && prop !== 'children') {
+      target.removeAttribute(prop)
+    }
+  })
+  if (Object.keys(newProps).length === 0) {
+    Object.keys(oldProps).forEach((prop) => {
+      if (prop !== 'children') {
+        target.setAttribute(prop, oldProps[prop])
+      }
+    })
+  }
 }
 
 export function render(parent, newNode, oldNode, index = 0) {
+  /*
   // 1. 만약 newNode가 없고 oldNode만 있다면
   //   parent에서 oldNode를 제거
-  //   종료
-
+  //   종료s
+  if (newNode === null && oldeNode !==null) {
+    parent.removeChild(parent.childNodes[index]);
+    return;
+  }
+      
   // 2. 만약 newNode가 있고 oldNode가 없다면
   //   newNode를 생성하여 parent에 추가
   //   종료
+  if(newNode !== null && oldNode === null) {
+    parent.appendChild(createElement(newNode));
+    return;
+  }
 
   // 3. 만약 newNode와 oldNode 둘 다 문자열이고 서로 다르다면
   //   oldNode를 newNode로 교체
   //   종료
-
+  if(typeof newNode !== typeof oldNode || (typeof newNode === 'string' && newNode !== oldNode)) {
+    if(typeof newNode === 'string') {(
+      parent.replaceChild(document.createTextNode(newNode), parent.childNodes[index]));
+    }else {
+      return;
+    } 
+    return;
+  } 
   // 4. 만약 newNode와 oldNode의 타입이 다르다면
   //   oldNode를 newNode로 교체
   //   종료
+  if(typeof newNode !== typeof oldNode) {
+    //oldNode.type = newNode.type;
+    parent.replaceChild(createElement(newNode), parent.childNodes[index]);
+    return;
+  }
 
   // 5. newNode와 oldNode에 대해 updateAttributes 실행
+  updateAttributes(parent.childNodes[index], newNode.props, oldNode.props);
 
   // 6. newNode와 oldNode 자식노드들 중 더 긴 길이를 가진 것을 기준으로 반복
   //   각 자식노드에 대해 재귀적으로 render 함수 호출
+  const length = Math.max(newNode.children.length, oldNode.children.length);
+   for(let i=0; i < length; i++) {
+    render(parent.childNodes[index], newNode.children[i], oldNode.children[i], i);
+  }*/
+  if (!oldNode) {
+    parent.appendChild(createElement(newNode))
+  } else if (!newNode) {
+    parent.removeChild(parent.childNodes[index])
+  } else if (isChanged(newNode, oldNode)) {
+    parent.replaceChild(createElement(newNode), parent.childNodes[index])
+  } else if (newNode.type) {
+    updateAttributes(parent.childNodes[index], newNode.props, oldNode.props)
+
+    const newLength = newNode.props.children.length
+    const oldLength = oldNode.props.children.length
+
+    for (let i = 0; i < newLength || i < oldLength; i++) {
+      render(parent.childNodes[index], newNode.props.children[i], oldNode.props.children[i], i)
+    }
+  }
+}
+
+function isChanged(node1, node2) {
+  return typeof node1 !== typeof node2 || (typeof node1 === 'string' && node1 !== node2) || node1.type !== node2.type
 }
