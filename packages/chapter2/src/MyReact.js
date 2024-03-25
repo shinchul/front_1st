600d09273d694d6b8c4d2213a53a46b40fd10589
brian90shin <brain90shin@gmail.com>
2024-03-25 11:09:28 +0900

    과제 C 제출

--- packages/chapter2/src/MyReact.js
+++ packages/chapter2/src/MyReact.js
@@ -2,8 +2,29 @@ import { createHooks } from "./hooks";
 import { render as updateElement } from "./render";
 
 function MyReact() {
-  const _render = () => {};
-  function render($root, rootComponent) {}
+  const renderContext = {
+    $root: null,
+    rootComponent: null,
+    currentNode: null,
+    beforeNode: null, //없어도됨 currentNode때문에
+  }
+
+  const _render = () => {
+    const currentNode = renderContext.rootComponent();
+
+    updateElement(renderContext.$root, currentNode, renderContext.currentNode);
+    resetHookContext();
+
+    renderContext.currentNode = currentNode;
+  };
+
+  function render($root, rootComponent) {
+    renderContext.$root = $root;
+    renderContext.rootComponent = rootComponent;
+    renderContext.currentNode = null;
+    renderContext.beforeNode = null;
+    _render();
+  }
 
   const { useState, useMemo, resetContext: resetHookContext } = createHooks(_render);
 
